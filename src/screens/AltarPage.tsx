b2e9f7e166cd4a7e87c52d6a66714d093ac8773b
Nguyen Hong Son <nguyenhongsont1lhp@gmail.com>
2026-07-28 23:45:50 +0700

    feat: implement Remix mode theme with interactive fireworks and thematic prayer modal

--- src/screens/AltarPage.tsx
+++ src/screens/AltarPage.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from 'react';
 import TechDeities, { DEITIES } from '../features/shrine/TechDeities';
 import CenserSection from '../features/censer/CenserSection';
